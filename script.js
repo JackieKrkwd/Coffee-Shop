6488c0ccdf931c9df3fc50f15ff890472e07f6c9
JackRie <riemersmajackie@gmail.com>
2020-08-05 14:29:48 -0400

    added menu items

--- script.js
+++ script.js
@@ -1,1 +1,89 @@
 "use strict";
+
+let menuItems = [
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+  {
+    name: "Coffee",
+    category: "drink",
+    description: "brown liquid",
+    price: 2,
+  },
+];
+
+let itemContainer = document.querySelector(".item-container");
+
+const display = () => {
+  itemContainer.innerHTML = "";
+  menuItems.forEach((item, index) => {
+    let card = document.createElement("div");
+    let nameParagraph = document.createElement("p");
+    nameParagraph.innerText = item.name;
+    let categoryParagraph = document.createElement("p");
+    categoryParagraph.innerText = item.category;
+    let descriptionParagraph = document.createElement("p");
+    descriptionParagraph.innerText = item.description;
+    let priceParagraph = document.createElement("p");
+    priceParagraph.innerText = item.price;
+    card.append(
+      nameParagraph,
+      categoryParagraph,
+      descriptionParagraph,
+      priceParagraph
+    );
+    itemContainer.append(card);
+  });
+};
+display();
