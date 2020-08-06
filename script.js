673be0597fbdefeb378e1c7bfb3858ef9ca70d91
Anthony Chiara <achiara12@gmail.com>
2020-08-06 10:11:19 -0400

    total feature for add item, delete in progress

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let menuItems = [
     price: 2,
   },
   {
+    image: 'https:images.app.goo.gl/LZmvocS43bYC5SW39',//img
     name: "Coffee",
     category: "drink",
     description: "brown liquid",
@@ -67,10 +68,19 @@ let itemContainer = document.querySelector(".item-container");
 let cartArray = [];
 let cartContainer = document.querySelector(".cart");
 
+//ADD TO CART
+let total = 0;
+let totalParagraph = document.querySelector(".total");
 itemContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("addToCart")) {
+    total = 0;
     let index = e.target.getAttribute("data-index");
     cartArray.push(menuItems[index]);
+    cartArray.forEach((product) => {
+      let amount = product.price;
+      total += amount;
+      totalParagraph.innerText = `Total: ${total}`;
+    })
     displayInCart();
   }
 });
@@ -105,13 +115,35 @@ display();
 
 const displayInCart = () => {
   cartContainer.innerHTML = "";
-  cartArray.forEach((item) => {
+  cartArray.forEach((item, index) => {
     let card = document.createElement("div");
     let nameParagraph = document.createElement("p");
     nameParagraph.innerText = item.name;
     let priceParagraph = document.createElement("p");
     priceParagraph.innerText = item.price;
-    card.append(nameParagraph, priceParagraph);
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.classList.add("delete");
+    deleteButton.setAttribute("data-index", index);
+    card.append(nameParagraph, priceParagraph, deleteButton);
     cartContainer.append(card);
   });
 };
+
+//REMOVES FROM CART WHEN YOU HIT DELETE
+cartContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    total = 0;
+    let ourIndex = e.target.getAttribute("data-index");
+    cartArray.splice(ourIndex, 1);
+    cartArray.forEach((product) => {
+      let amount = product.price;
+      total += amount;
+      //total -= product.price
+      totalParagraph.innerText = `Total: ${total}`;
+    });
+    displayInCart();
+  }
+});
+
+
